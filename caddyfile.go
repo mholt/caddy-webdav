58dc2147774e279ab77c7e2f0052c66df07a4664
Matthew Holt <mholt@users.noreply.github.com>
2022-09-16 22:39:33 -0600

    Add license
    Oops, forgot before

--- caddyfile.go
+++ caddyfile.go
@@ -1,3 +1,17 @@
+// Copyright 2015 Matthew Holt
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package webdav
 
 import (
@@ -22,10 +36,9 @@ func parseWebdav(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
 
 // UnmarshalCaddyfile sets up the handler from Caddyfile tokens.
 //
-//    webdav [<matcher>] {
-//        root <path>
-//    }
-//
+//	webdav [<matcher>] {
+//	    root <path>
+//	}
 func (wd *WebDAV) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
 		if d.NextArg() {
